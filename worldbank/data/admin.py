a6f555a672919c4849ab66bd9993ce2392cc3b07
Luccas Quadros <luccas.qdrs@gmail.com>
2020-01-09 15:11:26 -0300

    Importing data

--- worldbank/data/admin.py
+++ worldbank/data/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Country, Indicator, Stat
 # Register your models here.
+admin.site.register(Country)
+admin.site.register(Indicator)
+admin.site.register(Stat)
