65d0424ccc15e8a9eab6e76a8488961febd86878
Luccas Quadros <luccas.qdrs@gmail.com>
2020-01-12 16:04:16 -0300

    Changing readme, about and meta

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # World bank Data
 [Django](https://www.djangoproject.com/) + [Vue](https://vuejs.org/) project based on data from [World Bank Open Data](https://data.worldbank.org/)
+
+Usage
+-----
+You can start project in develop mode with
+[docker-compose](https://docs.docker.com/compose/):
+
+    $ docker-compose up --build
--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About this App</h1>
+    <p>
+      This is a little app done by
+      <a
+        href="https://github.com/luccasqdrs"
+        target="_blank"
+      >Luccas Quadros</a> using
+      <a href="https://www.djangoproject.com/" target="_blank">Django</a> and
+      <a href="https://vuejs.org" target="_blank">Vue</a>.
+    </p>
+    <p>For more information or contributing go to the  <a href="https://github.com/luccasqdrs/worldbank-data" target="_blank">project's repository</a></p>
   </div>
 </template>
